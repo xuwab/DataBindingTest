0da79db650c546a29bb84824d93413abd66f2116
xuwab <xuwab@outlook.com>
2016-10-09 21:30:39 +0800

    learning study of databinding

--- app/src/main/java/com/xuwab/databindingtest/MainActivity.java
+++ app/src/main/java/com/xuwab/databindingtest/MainActivity.java
@@ -1,24 +1,41 @@
 package com.xuwab.databindingtest;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.Toast;
 
 import com.xuwab.databindingtest.databinding.ActivityMainBinding;
+import com.xuwab.databindingtest.databinding.IncludeBinding;
+
+//import com.xuwab.databindingtest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private User user;
-
+//     final User user=new User("jone snow",23);
+    private ObservableArrayMap<String,Object> user=new ObservableArrayMap<>();
+    private   ActivityMainBinding binding;
+    private boolean isVisibility=false;
+    private IncludeBinding includeBinding;
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding=DataBindingUtil.setContentView(this,R.layout.activity_main);
-        user=new User("jone snow",23);
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
+        user.put("name","James");
+        user.put("age",24);
         binding.setUser(user);
         binding.setMain(this);
-
+        binding.vs.setOnInflateListener(new ViewStub.OnInflateListener() {
+            @Override
+            public void onInflate(ViewStub stub, View inflated) {
+                view=inflated;
+            }
+        });
     }
     public  String printTest1(){
         Toast.makeText(this,"Tom",Toast.LENGTH_LONG).show();
@@ -37,6 +54,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void printTestObservable(){
-
+//        user.setName("Hodo");
+//        Toast.makeText(MainActivity.this,user.getName(),Toast.LENGTH_LONG).show();
+    }
+    public void printTestObservableField(){
+//        user.mName.set("Sansa");
+    }
+    public void printTestObservableArrayMap(){
+        user.put("name","JamesBrother");
+        user.put("age",38);
+    }
+    public void showViewStub(){
+        if(!binding.vs.isInflated()){
+            binding.vs.getViewStub().inflate();
+        }else {
+            if(!isVisibility){
+                view.setVisibility(View.VISIBLE);
+            }else {
+                view.setVisibility(View.GONE);
+            }
+        }
+        isVisibility=!isVisibility;
     }
 }
--- app/src/main/java/com/xuwab/databindingtest/User.java
+++ app/src/main/java/com/xuwab/databindingtest/User.java
@@ -2,37 +2,48 @@ package com.xuwab.databindingtest;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.ObservableField;
 
 /**
  * Created by Administrator on 2016/10/8.
  */
 
-public class User extends BaseObservable{
-    private String name;
+//public class User extends BaseObservable{
+public class User {
+    public final ObservableField<String> mName=new ObservableField<>();
+    public final ObservableField<Integer> mAge=new ObservableField<>();
 
-    @Bindable
-    public String getName() {
-        return name;
+    public User(String name,int age) {
+        mName.set(name);
+        mAge.set(age);
     }
 
-    public void setName(String name) {
-        this.name = name;
-        notifyPropertyChanged(BR.name);
-    }
+    //    private String name;
+//    private int age;
 
-    public int getAge() {
-        return age;
-    }
+//    @Bindable
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//        notifyPropertyChanged(com.xuwab.databindingtest.BR.name);
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public User(String name, int age) {
+//
+//        this.name = name;
+//        this.age = age;
+//    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
-    public User(String name, int age) {
-
-        this.name = name;
-        this.age = age;
-    }
-
-    private int age;
 }
